3875d8d27d1afacc33eab9b8eb736b3e79406b70
Kostya Zvankovich <kostya.zvankovich@gmail.com>
2022-11-14 23:23:01 +0100

    add binary search trees

---
+++ bst/src/bst.clj
@@ -0,0 +1,29 @@
+(ns bst)
+
+(defn member
+  [x tree]
+  (loop [a tree]
+    (let [y (:data a)
+          left (:left a)
+          right (:right a)]
+      (if (empty? a)
+        false
+        (cond
+          (< x y) (recur left)
+          (> x y) (recur right)
+          :else true)))))
+
+(comment
+
+  (let [tree {:data 8
+              :left {:data 3
+                     :left {:data 1}
+                     :right {:data 6
+                             :left {:data 4}
+                             :right {:data 7}}}
+              :right {:data 10
+                      :right {:data 14
+                              :left {:data 13}}}}]
+    (member 61 tree)
+    )
+  )
